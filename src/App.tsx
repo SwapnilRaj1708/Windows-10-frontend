1727b17610d58eec3c89ee7af4e1d810fa50f5d2
Swapnil Raj <raj.swapnil1708@gmail.com>
2026-01-30 15:53:22 +0530

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/next"
 import Home from "./components/Home"
 import LockScreen from "./components/LockScreen/LockScreen"
 
 function App() {
   return (
     <>
+      <Analytics />
       <LockScreen />
       <Home />
     </>
